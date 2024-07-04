9ec1dccf167a928d5e0e2e66fa3f4a2727e2e511
Thiago Dias <thiaguinhodias.15@gmail.com>
2024-07-04 20:09:30 -0300

    chore: add trivia gama project

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -4,6 +4,7 @@ import StoreManager from '../assets/imgs/projects/store-manager.jpg';
 import CarShop from '../assets/imgs/projects/dealership.jpg';
 import BlogAPI from '../assets/imgs/projects/blog.jpg';
 import IDrink from '../assets/imgs/projects/idrink.png';
+import TriviaGame from '../assets/imgs/projects/TriviaGame.png';
 
 import { FaCss3Alt, FaDocker, FaHtml5, FaNodeJs, FaReact } from "react-icons/fa";
 import { SiExpress, SiJavascript, SiMysql, SiRedux, SiSequelize, SiSocketdotio, SiSwagger, SiTypescript } from "react-icons/si";
@@ -17,6 +18,13 @@ export const projectData = [
     content: 'Projeto Fullstack de e-commerce de bebidas, com área para clientes e vendedores.',
     tech: 'Tecnologias front-end: React.js, Redux, Axios. Testes: Jest & React Testing Library. Tecnologias back-end: Node.js, Express, JWT, Sequelize, Socket.io, Swagger. Testes: Mocha, Chai-Http & Sinon',
     techImgs: [<FaNodeJs className="icon" />, <SiExpress className="icon" />, <SiSequelize className="icon" />, <SiSocketdotio className="icon" />, <FaReact className="icon" />, <LuTestTube2 className="icon" />, <SiMysql className="icon" />, <SiSwagger className="icon" />, <FaDocker className="icon" />],
+    repository: 'https://github.com/zThiago15/IDrink',
+  },
+  {
+    image: TriviaGame,
+    title: 'Jogo de Trivia (Front-end)',
+    content: 'Jogo de Trivia, onde o usuário é apresentado com 5 questões com temporizador de 30 segundos para responder. Ao final é dado feedback de acordo com a quantidade de acertos e a opção para visualizar o Ranking dos participantes que obtiveram mais acertos.',
+    techImgs: [<FaReact className="icon" />, <SiRedux className="icon" />, <FaCss3Alt className="icon" />, <LuTestTube2 className="icon" />    ],
     repository: 'https://github.com/zThiago15/IDrink',
   },
   {
