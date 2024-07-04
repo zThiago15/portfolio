f252f94249192f3cfc12d5a676922ee7d130a1bc
Thiago Dias <thiaguinhodias.15@gmail.com>
2024-07-04 19:01:39 -0300

    feat: return tecnologies used in portfolio section

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { AiOutlineRight, AiOutlineLeft } from 'react-icons/ai';
 import { projectData } from '../../data/projectData';
 import './Project.css';
+import { FaFileAlt } from 'react-icons/fa';
 
 function Projects() {
 
@@ -19,35 +20,50 @@ function Projects() {
     setCurrent(firstImage ? length - 1 : current - 1);
   }
 
+  const generateId = (index) => {
+    return index + ((Math.random() * 100) + 1);
+  }
+
   return (
   <section className="portfolio section" id="portfolio">
-    <h2 className="section__title">Portfolio</h2>
+    <h2 className="section__title">Portfolio (Front-end & Back-end)</h2>
     <span className="section__subtitle">Meus projetos</span>
 
     <div className="portfolio__container container">
 
       <div className="slider">
         <AiOutlineLeft className="button__icon left-arrow" onClick={ prevSlide } />
+
           { projectData.map((project, index) => {
-            const { image, title, content, repository } = project;
+            const { image, title, content, repository, techImgs } = project;
             return (
               <div className={ index === current ? 'slide active' : 'slide' } key={ index } >
                 {
-                  index === current && (
+                  index === current && 
+                  (
                   <div className="portfolio__data">
                     <img src={ image } className="portfolio__img" />
                     <div className="portfolio__info">
+
                       <h3 className="portfolio__title">{ title }</h3>
                       <p className="portfolio__description">{ content }</p>
+
+                      <div key={generateId(index)} className="portfolio_tecnologies">
+                        {techImgs}
+                      </div>
+
                       <a href={ repository } target="_blank" className="button button--flex button--smal portfolio__button">
-                        Repositório
+                        <FaFileAlt id="icon__repository" /> Repositório
                       </a>
+
                     </div>
                   </div>)
+
                 }
               </div>
             );
           }) }
+
         <AiOutlineRight className="button__icon right-arrow" onClick={ nextSlide } />
 
 
--- src/data/projectData.js
+++ src/data/projectData.js
@@ -3,37 +3,56 @@ import TrybeWallet from '../assets/imgs/projects/trybe-wallet.png';
 import StoreManager from '../assets/imgs/projects/store-manager.jpg';
 import CarShop from '../assets/imgs/projects/dealership.jpg';
 import BlogAPI from '../assets/imgs/projects/blog.jpg';
+import IDrink from '../assets/imgs/projects/idrink.png';
+
+import { FaCss3Alt, FaDocker, FaHtml5, FaNodeJs, FaReact } from "react-icons/fa";
+import { SiExpress, SiJavascript, SiMysql, SiRedux, SiSequelize, SiSocketdotio, SiSwagger, SiTypescript } from "react-icons/si";
+import { LuTestTube2 } from "react-icons/lu";
+
 
 export const projectData = [
   {
+    image: IDrink,
+    title: 'IDrink (Front-end & Back-end)',
+    content: 'Projeto Fullstack de e-commerce de bebidas, com área para clientes e vendedores.',
+    tech: 'Tecnologias front-end: React.js, Redux, Axios. Testes: Jest & React Testing Library. Tecnologias back-end: Node.js, Express, JWT, Sequelize, Socket.io, Swagger. Testes: Mocha, Chai-Http & Sinon',
+    techImgs: [<FaNodeJs className="icon" />, <SiExpress className="icon" />, <SiSequelize className="icon" />, <SiSocketdotio className="icon" />, <FaReact className="icon" />, <LuTestTube2 className="icon" />, <SiMysql className="icon" />, <SiSwagger className="icon" />, <FaDocker className="icon" />],
+    repository: 'https://github.com/zThiago15/IDrink',
+  },
+  {
     image: SolarSystem,
-    title: 'Solar System',
+    title: 'Solar System(Front-end)',
     content: `Landpage de um modelo do sistema solar, mostrando seus planetas, 
     com tamanhos distintos, e informações sobre missões espaciais dos astronautas. Feito em React`,
+    techImgs: [<FaReact className="icon" />, <FaHtml5 className="icon" />, <FaCss3Alt className="icon" />, <SiJavascript className="icon" />],
     repository: 'https://github.com/zThiago15/solar-system'
   },
   {
     image: TrybeWallet,
-    title: 'Trybe Wallet',
+    title: 'Trybe Wallet(Front-end)',
     content: 'Aplicação para ter controle de seus gastos e despesas. Sendo possível criar, editar e removê-las. O saldo total aparecerá no canto da tela e pode-se converter a moeda, sendo utilizado uma API financeira para buscar o valor monetário atual de cada moeda. Feito em React.',
+    techImgs: [<FaReact className="icon" />, <FaHtml5 className="icon" />, <FaCss3Alt className="icon" />, <SiRedux className="icon" />],
     repository: 'https://github.com/zThiago15/wallet-expenses'
   },
   {
     image: StoreManager,
-    title: 'Store Manager',
+    title: 'Store Manager(Back-end)',
     content: 'API Rest de produtos e vendas para gerenciamento de uma loja. Feito com Node.js, express e testes unitários',
+    techImgs: [<FaNodeJs className="icon" />, <SiExpress className="icon" />, <SiMysql className="icon" />],
     repository: 'https://github.com/zThiago15/store-manager'
   }, 
   {
     image: CarShop,
-    title: 'Car Shop',
+    title: 'Car Shop(Back-end)',
     content: 'Gerenciamento de uma concessionária de carros e veículos',
+    techImgs: [<FaNodeJs className="icon" />, <SiTypescript className="icon" />, <SiExpress className="icon" />, <LuTestTube2 className="icon" />, <SiMysql className="icon" />],
     repository: 'https://github.com/zThiago15/car-shop'
   },
   {
     image: BlogAPI,
-    title: 'Blogs API',
+    title: 'Blogs API(Back-end)',
     content: 'API Rest de gerenciamento de um blog',
+    techImgs: [<FaNodeJs className="icon" />, <SiExpress className="icon" />, <SiSequelize className="icon" />, <FaDocker className="icon" />, <SiSwagger className="icon" />],
     repository: 'https://github.com/zThiago15/Blogs-API'
   }
 ];
